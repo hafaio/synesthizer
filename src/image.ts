1441e1efbaeae5175873e776f67efc9b9111de18
Erik Brinkman <erik.brinkman@gmail.com>
2026-07-04 23:09:46 -0400

    fix: reject convert on worker error

--- src/image.ts
+++ src/image.ts
@@ -1,17 +1,27 @@
 /** This file has utilities for turning an image into notes. */
 import type { Chord, Message, Result } from "./worker-interface";
 
-/** convert rgb to hsl */
+/** convert an image into playable chords via a background worker */
 export async function convert(
   img: ImageData,
   options: Omit<Message, "img">,
 ): Promise<Chord[]> {
   const message: Message = { img, ...options };
   const worker = new Worker(new URL("./worker.ts", import.meta.url));
-  const res = await new Promise<Result>((resolve) => {
+  const res = await new Promise<Result>((resolve, reject) => {
     worker.addEventListener("message", (event: MessageEvent<Result>) => {
       resolve(event.data);
       worker.terminate();
+    });
+    // a load/parse failure or bad message clone posts no message; reject so we
+    // don't hang forever
+    worker.addEventListener("error", (event) => {
+      worker.terminate();
+      reject(new Error(event.message || "worker failed to load"));
+    });
+    worker.addEventListener("messageerror", () => {
+      worker.terminate();
+      reject(new Error("worker message could not be deserialized"));
     });
     worker.postMessage(message);
   });
